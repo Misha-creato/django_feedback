3f1672c1901a57eee6223f273fcb1262f6189289
Nikolay Kiyanitsa <nikolay.kiyanitsa@gmail.com>
2024-04-12 21:33:58 +0500

    Created index app

---
+++ apps/index/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+
+from apps.feedback.models import Feedback
+
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        feedbacks = Feedback.objects.all()
+        context = {
+            'feedbacks': feedbacks,
+        }
+        return render(request=request, template_name='index.html', context=context)
