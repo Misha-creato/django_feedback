184caa77705dbef38bda2435b15088451ebb2af8
Iri O <oparina.iri@gmail.com>
2024-04-13 11:13:14 +0500

    Changed Feedback model

--- apps/feedback/models.py
+++ apps/feedback/models.py
@@ -3,17 +3,38 @@ from django.db import models
 
 class Feedback(models.Model):
     title = models.CharField(
-        max_length=128
+        max_length=128,
+        verbose_name='Заголовок',
     )
     text = models.TextField(
-        max_length=256
+        max_length=256,
+        verbose_name='Текст',
     )
     full_name = models.CharField(
-        max_length=128
+        max_length=128,
+        verbose_name='ФИО',
     )
     contacts = models.CharField(
-        max_length=64
+        max_length=64,
+        verbose_name='Контакты для связи',
+    )
+    is_resolved = models.BooleanField(
+        default=False,
+        verbose_name='Обработана',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Время создания',
     )
 
+    class Meta:
+        db_table = 'feedback'
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
+    @property
+    def status(self):
+        return 'Обработана' if self.is_resolved else 'В обработке'
+
     def __str__(self):
-        return f'{self.title}'
+        return self.title
