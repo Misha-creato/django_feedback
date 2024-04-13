f2f8832d88b5d38d4813e4a1c14eb7eabd5742f0
Iri O <oparina.iri@gmail.com>
2024-04-13 11:11:46 +0500

    Changed create_feedback func

--- apps/feedback/services.py
+++ apps/feedback/services.py
@@ -1,10 +1,16 @@
+from django.http import QueryDict
+from django.db import DatabaseError
+
 from feedback.models import Feedback
 
 
-def create_feedback_object(data):
-    Feedback.objects.create(
-        title=data['title'],
-        text=data['text'],
-        full_name=data['full_name'],
-        contacts=data['contacts'],
-    )
+def create_feedback_object(data: QueryDict):
+    try:
+        Feedback.objects.create(
+            title=data['title'],
+            text=data['text'],
+            full_name=data['full_name'],
+            contacts=data['contacts'],
+        )
+    except DatabaseError as ex:
+        print(f'Ошибка базы данных {ex}')
